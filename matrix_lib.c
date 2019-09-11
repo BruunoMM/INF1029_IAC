88e060cde3b4f4b02801cdbe3e944f9583b7032d
Bruno Marinho <brunommarinho@hotmail.com>
2019-09-11 15:50:30 -0300

    fix and make tests

--- matrix_lib.c
+++ matrix_lib.c
@@ -7,10 +7,39 @@ struct matrix {
     float *rows;
 };
 
-int main(void) {
+void test_scalar_matrix_mult();
+int scalar_matrix_mult(float scalar_value, struct matrix *matrix);
+void printMatrix(struct matrix *matrix);
 
-    // write scalar_matrix_mult tests
+int main(void) {
+    test_scalar_matrix_mult();
     return 0;
+}
+
+void test_scalar_matrix_mult() {
+    struct matrix testMatrix;
+    float *rows;
+
+    rows = malloc(8*sizeof(float));
+    rows[0] = 5.0;
+    rows[1] = 2.0;
+    rows[2] = 3.0;
+    rows[3] = 1.0;
+    rows[4] = 1.0;
+    rows[5] = 1.0;
+    rows[6] = 1.0;
+    rows[7] = 1.0;
+
+    testMatrix.height = 2;
+    testMatrix.width = 4;
+    testMatrix.rows = rows;
+    
+    printMatrix(&testMatrix);
+    printf("----\n");
+    int result = scalar_matrix_mult(2.0,&testMatrix);
+
+    printMatrix(&testMatrix);
+    free(rows);
 }
 
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix) {
@@ -25,10 +54,11 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix) {
         return 0;
     }
 
+    scalarVec = _mm256_set1_ps(scalar_value);
+
     for(int i=0; i < (height*width)/8; i++) {
         currentVec = _mm256_loadu_ps(currentRow);
-        scalarVec = _mm256_set1_ps(scalar_value);
-
+        
         // operation
         resultVec = _mm256_mul_ps(currentVec, scalarVec);
         
@@ -40,4 +70,16 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix) {
     }
 
     return 1;
+}
+
+void printMatrix(struct matrix *matrix) {
+    int width = matrix->width;
+    int height = matrix->height;
+
+    for(int i=1; i <= width * height; i++) {
+        printf("%.2f \t", matrix->rows[i-1]);
+        if(!(i % width)) {
+            printf("\n");
+        }
+    }
 }
